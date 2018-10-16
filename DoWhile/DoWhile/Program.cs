2fefd8943057ae286ac3c758597cab9ec2fc2337
Grant Parkis <gparkis@gmail.com>
2018-10-16 19:32:37 -0400

    final code

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+          
+            do
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            while (false);
+
+
+            Console.Read();
+        }
+    }
+}
